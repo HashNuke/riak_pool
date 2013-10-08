9b27443b66f4133fad033497a7052fc425b64ce1
Akash Manohar J <akash@akash.im>
2013-10-09 00:11:18 +0530

    Bump version to v0.2 and fix urls in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,11 +3,11 @@ defmodule RiakPool.Mixfile do
 
   def project do
     [ app: :riak_pool,
-      version: "0.1",
+      version: "0.2",
       elixir: "~> 0.10.3",
       name: "riak_pool",
-      source_url: "https://github.com/HashNuke/riak-pool",
-      homepage_url: "https://github.com/HashNuke/riak-pool",
+      source_url: "https://github.com/HashNuke/riak_pool",
+      homepage_url: "https://github.com/HashNuke/riak_pool",
       deps: deps ]
   end
 
