d4f09e82c4a42c72a51c5de1870e021813abb08c
Akash Manohar J <akash@akash.im>
2013-10-09 21:12:02 +0530

    Add package.exs

---
+++ package.exs
@@ -0,0 +1,8 @@
+Expm.Package.new(
+  name: "riak_pool",
+  description: "Provides pooled Riak connections, with auto-reconnect",
+  version: "0.2.1",
+  keywords: ["riak", "pool"], 
+  maintainers: [[name: "Akash Manohar", email: "akash@akash.im"]],
+  repositories: [[github: "HashNuke/riak_pool"]]
+)
