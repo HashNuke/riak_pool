f746ab0331c2ef67db24c78f53d46469055abda7
Akash Manohar J <akash@akash.im>
2013-11-10 11:16:30 +0530

    Update elixir, ex_doc and riakc

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RiakPool.Mixfile do
   def project do
     [ app: :riak_pool,
       version: "0.2.2",
-      elixir: "~> 0.10.3",
+      elixir: "~> 0.11.0",
       name: "riak_pool",
       source_url: "https://github.com/HashNuke/riak_pool",
       homepage_url: "https://github.com/HashNuke/riak_pool",
