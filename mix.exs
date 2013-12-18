e1e4e940d875deba3f4fba55c5b5aad4a6856b95
Akash Manohar J <akash@akash.im>
2013-12-18 22:04:00 +0530

    Update elixir version and ex_unit

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RiakPool.Mixfile do
   def project do
     [ app: :riak_pool,
       version: "0.2.2",
-      elixir: "~> 0.11.0",
+      elixir: ">= 0.12.0",
       name: "riak_pool",
       source_url: "https://github.com/HashNuke/riak_pool",
       homepage_url: "https://github.com/HashNuke/riak_pool",
